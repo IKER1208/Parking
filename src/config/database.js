95012f9bfddc78b497971ba62a3f51f19feeddac
IKER1208 <iker34267@gmail.com>
2025-03-18 12:39:10 -0600

    optimizacion, hagan lo mismo putos

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,18 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const {
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST
+} = process.env;
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: DB_HOST,
         dialect: 'mysql',
         logging: false
     }
