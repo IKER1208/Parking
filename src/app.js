25ce15af7df6899bd9a44efdc9fe1d168f9e9529
Piturrin22 <esteban.oz220106@gmail.com>
2025-03-21 19:38:50 -0600

    nuevas rutas para usuarios protegidas y todo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 
 sequelize.sync()
