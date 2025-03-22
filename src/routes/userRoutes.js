28272dcfc2c38519f3a93b19274220c7364bab66
Piturrin22 <esteban.oz220106@gmail.com>
2025-03-21 19:37:39 -0600

    nuevas rutas para usuarios protegidas y todo

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,3 +5,5 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/users', authMiddleware.authenticateJWT, authMiddleware.IsAdmin, userController.getAllUsers);
 router.post('/get/user', authMiddleware.authenticateJWT, authMiddleware.isClientOrAdmin, userController.getUserById);
+
+module.exports = router;
